2b5a9170202d69d5d0717955d687365366acbce6
Raheela <32760007+Raheela1024@users.noreply.github.com>
2022-01-12 13:39:24 +0000

    search autocomplete.

--- src/assets/js/d3tree.js
+++ src/assets/js/d3tree.js
@@ -459,7 +459,7 @@ treeJSON = d3.json(url, function(error, treeData) {
         div.transition()
             .duration(200)
             .style("opacity", .9);
-        div.html("<div id='doc' class='row' style='padding-top: 25px;'><div class='col-md-2'></div><div class='col-md-3'>Single click to expand or collapse a node</div><div class='col-md-3'>Double click to show Organisms table</div><div class='col-md-3'>Graph can be zoomed in & out and is draggable</div></div>")
+        // div.html("<div id='doc' class='row' style='padding-top: 25px;'><div class='col-md-2'></div><div class='col-md-3'>Single click to expand or collapse a node</div><div class='col-md-3'>Double click to show Organisms table</div><div class='col-md-3'>Graph can be zoomed in & out and is draggable</div></div>")
             // Transition nodes to their new position.
         var nodeUpdate = node.transition()
             .duration(duration)
@@ -604,6 +604,9 @@ treeJSON = d3.json(url, function(error, treeData) {
         //     $( "#project" ).val( ui.item.label );
         //     return false;
         // },
+        response: function (event, ui) {
+            $("#count").text('Founded ' + ui.content.length + ' results');
+        },
         select: function( event, ui ) {
             $( "#search" ).val( ui.item.label );
                 resetGraph();
@@ -613,7 +616,7 @@ treeJSON = d3.json(url, function(error, treeData) {
                 var paths = searchTree(root,ui.item.label,[]);
                 if(typeof(paths) !== "undefined"){
                     openPaths(paths);
-
+                    $("#count").text('');
                 }
                 else{
                     alert(ui.item.label+" not found!");
