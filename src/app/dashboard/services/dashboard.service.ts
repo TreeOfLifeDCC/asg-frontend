e3a909a6e911cc65bb25c834a60c52aa475dd044
Raheela <32760007+Raheela1024@users.noreply.github.com>
2022-10-31 09:40:28 +0000

    local url reverted.

--- src/app/dashboard/services/dashboard.service.ts
+++ src/app/dashboard/services/dashboard.service.ts
@@ -13,9 +13,9 @@ import {tap} from 'rxjs/operators';
 })
 export class DashboardService {
 
-  // private API_BASE_URL = 'https://portal.aquaticsymbiosisgenomics.org/api';
+  private API_BASE_URL = 'https://portal.aquaticsymbiosisgenomics.org/api';
   // private API_BASE_URL = 'http://45.88.81.97/backend';
-  private API_BASE_URL = 'http://localhost:8080';
+  // private API_BASE_URL = 'http://localhost:8080';
   private ENA_PORTAL_API_BASE_URL = 'https://www.ebi.ac.uk/ena/portal/api/files';
 
   constructor(private http: HttpClient, private bytesPipe: BytesPipe,  private dialog: MatDialog) { }
