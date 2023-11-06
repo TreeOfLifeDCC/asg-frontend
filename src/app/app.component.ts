a9bc9946b3c47232c597b78b8eb6422c4033626e
Alexey Sokolov <alexey@ebi.ac.uk>
2023-11-06 12:45:21 +0000

    Updating cookie-consent module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,34 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import {NgcCookieConsentService, NgcCookieConsentConfig} from 'ngx-cookieconsent';
+
+export const cookieConfig:NgcCookieConsentConfig =   {
+  "cookie": {
+    "domain": "portal.aquaticsymbiosisgenomics.org"
+  },
+  "position": "bottom",
+  "theme": "classic",
+  "palette": {
+    "popup": {
+      "background": "#333",
+      "text": "#ffffff",
+      "link": "#ffffff"
+    },
+    "button": {
+      "background": "#f1d600",
+      "text": "#333",
+      "border": "transparent"
+    }
+  },
+  "type": "info",
+  "content": {
+    "message": "This website requires cookies, and the limited processing of your personal data in order to function. By using the site you are agreeing to this as outlined in our",
+    "dismiss": "Accept cookies",
+    "deny": "Refuse cookies",
+    "link": "Privacy Notice",
+    "href": "https://www.ebi.ac.uk/data-protection/privacy-notice/darwin-tree-of-life-data-portal",
+    "policy": "Cookie Policy"
+  }
+};
 
 @Component({
   selector: 'app-root',
@@ -9,6 +39,9 @@ export class AppComponent {
   title = 'asg-portal';
   @ViewChild('cookieLaw')
   cookieLawEl: any;
+
+  constructor(private ccService: NgcCookieConsentService) {
+  }
 
   ngOnInit() {
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent, cookieConfig} from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // import { CookieLawModule } from 'angular2-cookie-law';
@@ -10,7 +10,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { MatTreeModule } from '@angular/material/tree';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 // import { MatTableExporterModule } from 'mat-table-exporter';
 
 import { HeaderComponent } from './shared/header/header.component';
@@ -21,7 +20,6 @@ import { ApiComponent } from './api/api.component';
 import {PhylogeneticsModule} from './phylogenetics/phylogenetics.module';
 import {ConfirmationDialogComponent} from './confirmation-dialog-component/confirmation-dialog.component';
 import {BytesPipe} from './shared/bytes-pipe';
-import {MatLegacyDialogModule as MatDialogModule} from '@angular/material/legacy-dialog';
 import {GisComponent} from './gis/gis.component';
 import {GisService} from './gis/gis.service';
 import {FilterService} from "./services/filter-service";
@@ -29,15 +27,18 @@ import {FilterComponent} from './shared/filter/filter.component';
 import {ActiveFilterComponent} from './shared/active-filter/active-filter.component';
 import {PhylogenyFilterComponent} from './shared/phylogeny-filter/phylogeny-filter.component';
 import {NgxSpinnerModule} from 'ngx-spinner';
-import {MatLegacyAutocompleteModule as MatAutocompleteModule} from '@angular/material/legacy-autocomplete';
-import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
-import {MatLegacySlideToggleModule as MatSlideToggleModule} from '@angular/material/legacy-slide-toggle';
-import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
-import {MatLegacyRadioModule as MatRadioModule} from '@angular/material/legacy-radio';
-import {MatLegacyTooltipModule as MatTooltipModule} from '@angular/material/legacy-tooltip';
 import {BulkDownloadsComponent} from './bulk-downloads/bulk-downloads.component';
 import {DownloadConfirmationDialogComponent} from './download-confirmation-dialog-component/download-confirmation-dialog.component';
 import {DashboardModule} from './dashboard/dashboard.module';
+import {NgcCookieConsentModule} from "ngx-cookieconsent";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatInputModule} from "@angular/material/input";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @NgModule({
   declarations: [
@@ -74,7 +75,8 @@ import {DashboardModule} from './dashboard/dashboard.module';
     MatInputModule,
     MatRadioModule,
     MatTooltipModule,
-    DashboardModule
+    DashboardModule,
+    NgcCookieConsentModule.forRoot(cookieConfig)
   ],
   providers: [BytesPipe, GisService, FilterService],
   bootstrap: [AppComponent]
