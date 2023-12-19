3497bbe54638bb0496bafcb2ab19e3ffbe23cd2d
Yogmatee Roochun <yroochun@ebi.ac.uk>
2023-12-19 14:06:10 +0000

    updating angular version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,32 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {NgcCookieConsentService, NgcCookieConsentConfig} from 'ngx-cookieconsent';
 
-export const cookieConfig:NgcCookieConsentConfig =   {
-  "cookie": {
-    "domain": "portal.aquaticsymbiosisgenomics.org"
+export const cookieConfig: NgcCookieConsentConfig =   {
+  cookie: {
+    domain: 'portal.aquaticsymbiosisgenomics.org'
   },
-  "position": "bottom",
-  "theme": "classic",
-  "palette": {
-    "popup": {
-      "background": "#333",
-      "text": "#ffffff",
-      "link": "#ffffff"
+  position: 'bottom',
+  theme: 'classic',
+  palette: {
+    popup: {
+      background: '#333',
+      text: '#ffffff',
+      link: '#ffffff'
     },
-    "button": {
-      "background": "#f1d600",
-      "text": "#333",
-      "border": "transparent"
+    button: {
+      background: '#f1d600',
+      text: '#333',
+      border: 'transparent'
     }
   },
-  "type": "info",
-  "content": {
-    "message": "This website requires cookies, and the limited processing of your personal data in order to function. By using the site you are agreeing to this as outlined in our",
-    "dismiss": "Accept cookies",
-    "deny": "Refuse cookies",
-    "link": "Privacy Notice",
-    "href": "https://www.ebi.ac.uk/data-protection/privacy-notice/darwin-tree-of-life-data-portal",
-    "policy": "Cookie Policy"
+  type: 'info',
+  content: {
+    message: 'This website requires cookies, and the limited processing of your personal data in order to function. By using the site you are agreeing to this as outlined in our',
+    dismiss: 'Accept cookies',
+    deny: 'Refuse cookies',
+    link: 'Privacy Notice',
+    href: 'https://www.ebi.ac.uk/data-protection/privacy-notice/darwin-tree-of-life-data-portal',
+    policy: 'Cookie Policy'
   }
 };
 
@@ -37,7 +37,7 @@ export const cookieConfig:NgcCookieConsentConfig =   {
 })
 export class AppComponent {
   title = 'asg-portal';
-  @ViewChild('cookieLaw')
+  // @ViewChild('cookieLaw')
   cookieLawEl: any;
 
   constructor(private ccService: NgcCookieConsentService) {
@@ -46,7 +46,7 @@ export class AppComponent {
   ngOnInit() {
   }
 
-  dismiss(): void {
-    this.cookieLawEl.dismiss();
-  }
+  // dismiss(): void {
+  //   this.cookieLawEl.dismiss();
+  // }
 }
