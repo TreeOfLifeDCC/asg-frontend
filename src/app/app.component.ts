de5a23a84dc0cbeada3f9d481f5ed2605f18be04
yroochun <yroochun@ebi.ac.uk>
2025-04-25 12:41:12 +0100

    added terms of use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,13 +22,22 @@ export const cookieConfig: NgcCookieConsentConfig = {
     }
   },
   type: 'info',
+  elements:{
+    messagelink: `<span id="cookieconsent:desc" class="cc-message">{{message}}
+                        <a aria-label="learn more about our privacy policy" tabindex="1" class="cc-link" href="{{privacyPolicyHref}}" target="_blank" rel="noopener">{{privacyPolicyLink}}</a> and
+                        <a aria-label="learn more about our terms of service" tabindex="2" class="cc-link" href="{{tosHref}}" target="_blank" rel="noopener">{{tosLink}}</a>
+                    </span>
+                    `,
+  },
   content: {
     message: 'This website requires cookies, and the limited processing of your personal data in order to function. By using the site you are agreeing to this as outlined in our',
     dismiss: 'Accept cookies',
-    deny: 'Refuse cookies',
-    link: 'Privacy Notice',
-    href: '/assets/gdpr/erga_asg_gbdp_gdpr.pdf',
-    policy: 'Cookie Policy'
+
+    privacyPolicyLink: 'Privacy Notice',
+    privacyPolicyHref: '/assets/gdpr/erga_asg_gbdp_gdpr.pdf',
+
+    tosLink: 'Terms of Use',
+    tosHref: '/assets/gdpr/terms_of_use.pdf',
   }
 };
 
