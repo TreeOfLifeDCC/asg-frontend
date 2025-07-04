ed497bf9b666a661e9e7a20a8b18857e0d94fd4c
Raheelaaslam1024 <160017141+Raheelaaslam1024@users.noreply.github.com>
2025-07-04 11:48:28 +0100

    Species groups filters added.

--- src/app/dashboard/component/dashboard.component.ts
+++ src/app/dashboard/component/dashboard.component.ts
@@ -155,6 +155,7 @@ export class DashboardComponent implements OnInit, AfterViewInit , OnDestroy {
   metagenomesFilters: any[] = [];
   experimentTypeFilters: any[] = [];
   mgnifyFilters: any[] = [];
+  asgSpeciesGroup: any[] = [];
   itemLimit = 5;
   isCollapsed = true;
   searchValue: string;
@@ -404,6 +405,12 @@ export class DashboardComponent implements OnInit, AfterViewInit , OnDestroy {
                     'experimentType');
               }
 
+              this.asgSpeciesGroup = [];
+              if (this.aggregations?.asg_species_group.buckets.length > 0) {
+                this.asgSpeciesGroup = this.merge(this.asgSpeciesGroup,
+                    this.aggregations?.asg_species_group.buckets,
+                    'asg_species_group');
+              }
 
               // get last phylogeny element for filter button
               this.lastPhylogenyVal = this.phylogenyFilters.slice(-1)[0];
@@ -648,6 +655,10 @@ export class DashboardComponent implements OnInit, AfterViewInit , OnDestroy {
       return filterName;
     }
     switch (true) {
+      case filterName.startsWith('asg_species_group'):
+        return 'Species groups-' + filterName.split('-')[1];
+      case filterName.startsWith('metagenomes'):
+        return 'Metagenomes-' + filterName.split('-')[1];
       case filterName.startsWith('symbionts_'):
         return 'Symbionts-' + filterName.split('-')[1];
       case filterName.startsWith('experimentType_'):
